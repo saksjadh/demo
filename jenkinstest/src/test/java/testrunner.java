f8cc7fd2f1bfbb54ab25438fb1373752f062a5c9
saksjadh <saksjadh@INLEN8520046726.corp.capgemini.com>
2022-10-07 10:53:52 +0530

    First commit

---
+++ jenkinstest/src/test/java/testrunner.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testrunner {
+
+	@Test
+	public void test() {
+		System.out.print("Hi user, I am jenkins proj");
+	}
+
+}
